9ee6c210876192a13fcb5a0ed1ac6f0f024332dd
LiptonLeon <krystian.baraniecki@gmail.com>
2022-03-12 01:22:16 +0100

    Basic GUI structure

--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
@@ -11,8 +11,8 @@ public class EgsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EgsApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("ElGamal Signature App");
         stage.setScene(scene);
         stage.show();
     }
