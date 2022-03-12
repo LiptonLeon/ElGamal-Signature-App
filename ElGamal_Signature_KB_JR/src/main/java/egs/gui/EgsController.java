4ec20005eb9f9640dc190214594839e1ee97f0e2
LiptonLeon <krystian.baraniecki@gmail.com>
2022-03-12 12:59:31 +0100

    Added essential nodes to GUI

--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsController.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsController.java
@@ -1,14 +1,22 @@
 package egs.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 
 public class EgsController {
+
+    @FXML
+    private Button testButton;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onTestButtonClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.showSaveDialog(testButton.getScene().getWindow());
     }
 }
