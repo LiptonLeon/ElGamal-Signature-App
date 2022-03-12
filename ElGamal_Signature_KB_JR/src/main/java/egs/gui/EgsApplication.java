5a5d78013b3d445b07d05060f0dff188bf9a3313
LiptonLeon <krystian.baraniecki@gmail.com>
2022-03-12 16:57:54 +0100

    Finalized GUI style

--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
@@ -11,7 +11,7 @@ public class EgsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EgsApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("ElGamal Signature App");
         stage.setScene(scene);
         stage.show();
