e344f0d7427fc8a28547973649ae09fa9ab99c3e
LiptonLeon <krystian.baraniecki@gmail.com>
2022-03-14 22:15:09 +0100

    Open/save dialog, file path gets updated

--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
@@ -3,6 +3,7 @@ package egs.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,10 +15,10 @@ public class EgsApplication extends Application {
         System.setProperty("prism.lcdtext", "false");
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setOpacity(0.95);
-        stage.setTitle("ElGamal Signature App");
 
         FXMLLoader fxmlLoader = new FXMLLoader(EgsApplication.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         ((EgsController) fxmlLoader.getController()).setStage(stage);
         stage.show();
--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsController.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsController.java
@@ -1,15 +1,25 @@
 package egs.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class EgsController {
 
     @FXML
-    private Button testButton;
+    private Label textPath;
 
+    @FXML
+    private Label signPath;
+
+    @FXML
+    private Label keyPath;
+
+    private final FileChooser fileChooser = new FileChooser();
     private Stage stage;
 
     // FXResizeHelper can be initialized after passing stage to controller
@@ -33,4 +43,51 @@ public class EgsController {
         stage.setIconified(true);
     }
 
+    public void onTextLoad(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            //TODO: Load text
+            textPath.setText(file.getPath());
+        }
+    }
+
+    public void onTextSave(ActionEvent actionEvent) {
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            //TODO: Save text
+            textPath.setText(file.getPath());
+        }
+    }
+
+    public void onSignLoad(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            //TODO: Load signature
+            signPath.setText(file.getPath());
+        }
+    }
+
+    public void onSignSave(ActionEvent actionEvent) {
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            //TODO: Save signature
+            signPath.setText(file.getPath());
+        }
+    }
+
+    public void onKeyLoad(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            //TODO: Load keys
+            keyPath.setText(file.getPath());
+        }
+    }
+
+    public void onKeySave(ActionEvent actionEvent) {
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            //TODO: Save keys
+            keyPath.setText(file.getPath());
+        }
+    }
 }
