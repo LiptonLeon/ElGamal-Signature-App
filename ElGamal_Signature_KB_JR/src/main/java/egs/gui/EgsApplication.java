9d4479615c6968926d85f6fde2607e9e2b7e947e
236496 Baraniecki Krystian <236496@edu.p.lodz.pl>
2022-03-14 13:30:40 +0100

    Changed GUI layout

--- ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
+++ ElGamal_Signature_KB_JR/src/main/java/egs/gui/EgsApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class EgsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(EgsApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(EgsApplication.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("ElGamal Signature App");
         stage.setScene(scene);
