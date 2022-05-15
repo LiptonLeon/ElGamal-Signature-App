2bac42fad6c84db67a54a5042f77eac3b766a10d
LiptonLeon <krystian.baraniecki@gmail.com>
2022-05-15 23:36:00 +0200

    Polski (w sensie polish)

--- ElGamalSignature/src/main/java/egs/gui/PopupController.java
+++ ElGamalSignature/src/main/java/egs/gui/PopupController.java
@@ -17,4 +17,62 @@ public class PopupController {
         ((Stage) infoLabel.getScene().getWindow()).close();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void setStr(String str) {
+        switch (str) {
+            case "kapsle" -> setInfo("Kapsle racing:\nliptonleon.itch.io/\nkapsle-racing");
+            case "418" -> setInfo("I'm a teapot!");
+            case "studia" -> setInfo("Kto normalny daje\neaster eggi do\nprogramu na zaliczenie?");
+        }
+    }
 }
